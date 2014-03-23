1cec41e9a410c2960a249a05a6fa2b213fb0e0bc
Amine Amanzou <amine@lacompany.fr>
2014-03-23 20:42:04 +0100

    Working on border layout positioning

--- L3APP-TD7-IHM/src/Main.java
+++ L3APP-TD7-IHM/src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+import static java.awt.BorderLayout.*;
 import javax.swing.*;
 
 /**
@@ -11,12 +13,37 @@ public class Main {
      */
     public static void main(String[] args) {
         Fenetre f = new Fenetre();
-        JLabel label = new JLabel("Label");
-        JButton button = new JButton("Button");
-        JPanel panelSup = new JPanel();
-        f.setContentPane(panelSup);
-        panelSup.add(label);
-        panelSup.add(button);
+        
+        JLabel title = new JLabel("TITRE");
+        JLabel jour = new JLabel("JOUR");
+        JLabel nuit = new JLabel("NUIT");
+        JLabel jour1 = new JLabel("JOUR");
+        JLabel nuit1 = new JLabel("NUIT");
+        JLabel jour2 = new JLabel("JOUR");
+        JLabel nuit2 = new JLabel("NUIT");
+        JButton button = new JButton("OK");
+
+        BorderLayout layout = new BorderLayout();
+        GridLayout grid = new GridLayout(4,1);
+        
+        JPanel panel = new JPanel();
+        JPanel panelEst = new JPanel();
+        panel.setLayout(layout);
+        panelEst.setLayout(grid);
+        
+        f.setContentPane(panel);
+        panel.add(title,NORTH);
+        panel.add(button,SOUTH);
+        panel.add(jour,WEST);
+        panel.add(nuit,CENTER);
+        panel.add(panelEst,EAST);
+        panelEst.add(jour1);
+        panelEst.add(nuit1);
+        panelEst.add(jour2);
+        panelEst.add(nuit2);
+        
+        f.revalidate();
+        f.repaint();
     }
     
 }
