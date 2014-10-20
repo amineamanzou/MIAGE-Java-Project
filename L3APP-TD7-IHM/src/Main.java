ad3cb587f1ee3630f73a47bf5c7fe427b5500712
Amine Amanzou <amineamanzou@gmail.com>
2014-10-21 00:11:40 +0200

    Last IHM coding for the L3

--- L3APP-TD7-IHM/src/Main.java
+++ L3APP-TD7-IHM/src/Main.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import static java.awt.BorderLayout.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 /**
@@ -21,7 +23,24 @@ public class Main {
         JLabel nuit1 = new JLabel("NUIT");
         JLabel jour2 = new JLabel("JOUR");
         JLabel nuit2 = new JLabel("NUIT");
-        JButton button = new JButton("OK");
+        final JButton button = new JButton("OK");
+        button.addActionListener(new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e)
+            {
+                Object source = e.getSource();
+                if(source == button){
+                    if(button.getText().equals("OK")){
+                        button.setText("Clicked");
+                    } else {
+                        button.setText("OK");
+                    }
+                }
+                //Execute when button is pressed
+                System.out.println("You clicked the button");
+            }
+        });      
 
         BorderLayout layout = new BorderLayout();
         GridLayout grid = new GridLayout(4,1);
