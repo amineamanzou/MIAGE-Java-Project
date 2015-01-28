d20e6ab7efaaaef25ca0f61c78153d1009e9f014
Duthz <pierre.gaillard89140@gmail.com>
2015-01-28 15:09:37 +0100

    Checking connexion, random draw

--- M1APP-Network/Game/src/Server.java
+++ M1APP-Network/Game/src/Server.java
@@ -4,33 +4,38 @@ import java.io.OutputStreamWriter;
 import java.io.Writer;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.math.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 /**
  *
  * @author pierregaillard
  */
 public class Server {
-
     public static void main(String[] args) {
+        double draw = (Math.random()*100) % 2;
+        int start = (int) draw; //forcément égal à 0 ou 1. 0 => Client commence, 1 Serveur Commence. 
+        System.out.println(start);
         ServerSocket ecoute = null;
         Socket s = null;
         Writer out = null;
+        int port = Integer.parseInt(args[0]);
+        int nb = 0;
         try {
-            ecoute = new ServerSocket(12000);
-            s = ecoute.accept();
-            out = new BufferedWriter((new OutputStreamWriter(s.getOutputStream())));
-            
-            // Partie logique à faire côté server
-            /* out.write("Salut à tous \r\n");
-            out.write("A plus tard ! \r\n");
-            out.flush(); */
-            
-            s.close();
+            ecoute = new ServerSocket(port);
+            System.out.println("Lancement du Serveur sur le port : "+port+" !");
+            while (true) {
+                s = ecoute.accept();
+                System.out.println("Connexion avec : "+ecoute.getInetAddress()+" réussi !");
+                out = new BufferedWriter((new OutputStreamWriter(s.getOutputStream())));
+                // Partie logique à faire côté server
+                
+                s.close();
+            }
         } catch (Exception e) {
             System.out.println("Erreur ahah");
         }
